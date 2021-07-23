4696b022ac01ea45b6420301a6ac384756781626
sagar <sean.lu91@gmail.com>
2021-07-23 08:07:32 +0000

    add test code for blockSet

---
+++ contract/blockset/test/hardhat.config.js
@@ -0,0 +1,41 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+//task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//  const accounts = await hre.ethers.getSigners();
+//
+//  for (const account of accounts) {
+//    console.log(account.address);
+//  }
+//});
+
+// Define mnemonic for accounts.
+let mnemonic = process.env.MNEMONIC;
+if (!mnemonic) {
+	// NOTE: this fallback is for development only!
+	// When using other networks, set the secret in .env.
+	// DO NOT commit or share your mnemonic with others!
+	mnemonic = 'test test test test test test test test test test test test';
+}
+
+const accounts = { mnemonic };
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+	solidity: "0.5.1",
+	networks: {
+		hardhat: {
+			accounts,
+		},
+		testnet: {
+			url: 'http://114.242.26.15:8006',
+			accounts,
+		},
+	},
+};
---
+++ contract/blockset/test/test/block-test.js
@@ -0,0 +1,109 @@
+const { expect } = require("chai");
+
+describe("BlockSet", function () {
+  it("Should got 0 admins", async function () {
+    const BlockSet = await ethers.getContractFactory("blockSet");
+    const blockSet = await BlockSet.deploy();
+    await blockSet.deployed();
+	  var admins = await blockSet.getAdmins();
+	  expect(admins.length == 0);
+  });
+  it("Should got 1 admins", async function () {
+    const BlockSet = await ethers.getContractFactory("blockSet");
+    const blockSet = await BlockSet.deploy();
+    await blockSet.deployed();
+
+    const addAdminTx = await blockSet.addAdmin("0x69DC6E2990C73B658DcbAB841c630F082AAAeD2D");
+
+    // wait until the transaction is mined
+    await addAdminTx.wait();
+	  var admins = await blockSet.getAdmins();
+	  expect(admins.length == 1);
+  });
+  it("Should delete to 0 admins", async function () {
+    const BlockSet = await ethers.getContractFactory("blockSet");
+    const blockSet = await BlockSet.deploy();
+    await blockSet.deployed();
+
+    const addAdminTx = await blockSet.addAdmin("0x69DC6E2990C73B658DcbAB841c630F082AAAeD2D");
+
+    // wait until the transaction is mined
+    await addAdminTx.wait();
+	  var admins = await blockSet.getAdmins();
+	  expect(admins.length == 1);
+
+    const delAdminTx = await blockSet.deleteAdmin("0x69DC6E2990C73B658DcbAB841c630F082AAAeD2D");
+
+    // wait until the transaction is mined
+    await delAdminTx.wait();
+	  var admins = await blockSet.getAdmins();
+	  expect(admins.length == 0);
+  });
+  it("Should change owner", async function () {
+    const BlockSet = await ethers.getContractFactory("blockSet");
+    const blockSet = await BlockSet.deploy();
+    await blockSet.deployed();
+
+    const changeOwnerTx= await blockSet.changeOwner("0x69DC6E2990C73B658DcbAB841c630F082AAAeD2D");
+
+    // wait until the transaction is mined
+    await changeOwnerTx.wait();
+	  var owner = await blockSet.getOwner();
+	  console.log("got owner",owner);
+	  //expect(owner.equal("0x69DC6E2990C73B658DcbAB841c630F082AAAeD2D"));
+  });
+  it("Should change threshold", async function () {
+    const BlockSet = await ethers.getContractFactory("blockSet");
+    const blockSet = await BlockSet.deploy();
+    await blockSet.deployed();
+	  var initThreshold = await blockSet.getThreshold();
+	  expect(initThreshold==1);
+
+    const addAdminTx1 = await blockSet.addAdmin("0x69DC6E2990C73B658DcbAB841c630F082AAAeD2D");
+    await addAdminTx1.wait();
+    const addAdminTx2 = await blockSet.addAdmin("0xcfCb3cB30C940614e18A3b53Ae9fa91a8592f417");
+    await addAdminTx2.wait();
+	  var admins = await blockSet.getAdmins();
+	  //console.log(admins);
+	  expect(admins.length == 2);
+
+    const setTx= await blockSet.setThreshold(2);
+    await setTx.wait();
+	  var threshold = await blockSet.getThreshold();
+	  expect(threshold==2);
+  });
+  it("Should got value ", async function () {
+    const BlockSet = await ethers.getContractFactory("blockSet");
+    const blockSet = await BlockSet.deploy();
+    await blockSet.deployed();
+	  const addProposalTx = await blockSet.addProposal("test1",1000);
+	  await addProposalTx.wait();
+
+	  var test1 = await blockSet.getValue("test1");
+	  expect(test1==1000);
+  });
+  it("Should revert", async function () {
+    const BlockSet = await ethers.getContractFactory("blockSet");
+    const blockSet = await BlockSet.deploy();
+    await blockSet.deployed();
+	  var initThreshold = await blockSet.getThreshold();
+	  expect(initThreshold==1);
+
+    const addAdminTx1 = await blockSet.addAdmin("0x69DC6E2990C73B658DcbAB841c630F082AAAeD2D");
+    await addAdminTx1.wait();
+    const addAdminTx2 = await blockSet.addAdmin("0xcfCb3cB30C940614e18A3b53Ae9fa91a8592f417");
+    await addAdminTx2.wait();
+    var admins = await blockSet.getAdmins();
+    expect(admins.length == 2);
+
+    const setTx= await blockSet.setThreshold(2);
+    await setTx.wait();
+    var threshold = await blockSet.getThreshold();
+    expect(threshold==2);
+
+    const addProposalTx = await blockSet.addProposal("test1",1000);
+    await addProposalTx.wait();
+  
+    await expect(blockSet.getValue("test1")).to.revertedWith("VM Exception while processing transaction: reverted with reason string 'not valid'")
+  });
+});
