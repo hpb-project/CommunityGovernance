a877bc6fdfdf5b9eabba5b6bae7d7bb81e777954
sagar <sean@gmail.com>
2021-07-31 19:42:34 +0800

    add lockandvote test

---
+++ contract/lockandvote/test/contracts/lock.sol
@@ -0,0 +1,95 @@
+pragma solidity ^0.5.1;
+import "./nodes.sol";
+import "./safemath.sol";
+
+contract HpbLock is Ownable{
+    using SafeMath for uint256;
+    uint lockNum=30000 ether;//默认每个账户只能锁定3万
+    mapping(address=>uint) public lockBal;//保存质押金额
+    mapping(address=>address payable) public nodeToLockAddr;//节点地址=》质押地址
+ 
+    HpbNodes boenodes;
+    address payable nodeaddr;
+    
+    function setNodeContract(address payable addr) onlyAdmin public {
+        boenodes = HpbNodes(addr);
+        nodeaddr = addr;
+    }
+
+    function getNodeContract() public view returns(address){
+        return nodeaddr;
+    }
+    /**
+	 ** 质押锁仓，动态选举可随时进行质押
+	 */
+    function stake(address payable nodeAddr) public payable returns (bool){
+        require(msg.value>=lockNum,"value too low");//至少锁仓30000个HPB
+        require(lockBal[nodeAddr]==0,"locked");//锁仓数量0代表还没锁仓
+        require(boenodes.isBoeNode(nodeAddr),"not boe");
+        lockBal[nodeAddr]=lockNum;//记录下锁仓金额
+        nodeToLockAddr[nodeAddr]=msg.sender;//记录节点地址对应的质押地址
+        boenodes.stake(nodeAddr);
+        if(msg.value > lockNum){//如果锁仓超过30000个HPB，多余的退回
+            msg.sender.transfer(msg.value.sub(lockNum));
+        }
+        emit LockBal(nodeAddr,msg.sender,lockNum);
+        return true;
+    }
+    /**
+	 ** 解锁质押,动态选举中可随时解除质押
+	 */
+    function withdraw(address payable nodeAddr) public payable returns (bool){
+        require(nodeToLockAddr[nodeAddr]==msg.sender,"node nq sender");//必须是节点地址对应的质押地址
+        require(lockBal[nodeAddr]==lockNum);//必须是已经锁仓3万个HPB
+        lockBal[nodeAddr]=0;//重置锁仓为0，代表已经全部解锁
+        delete nodeToLockAddr[nodeAddr];
+        boenodes.withdraw(nodeAddr);
+        msg.sender.transfer(lockNum);//解锁即可取回锁仓的金额
+        emit withdrawBal(nodeAddr,msg.sender,lockNum);
+        return true;
+    }
+    
+    function fetchLockInfoByNodeAddr(address nodeAddr) public view returns (
+        address,
+        uint
+    ) {
+        return (nodeToLockAddr[nodeAddr],lockBal[nodeAddr]);
+    }
+    
+    constructor (
+    ) payable public {
+        owner = msg.sender;
+        addAdmin(owner);
+    }
+    function () payable external {
+        emit ReceivedHpb(
+            msg.sender,
+            msg.value
+        );
+        if(msg.value>0){
+            require(stake(msg.sender));
+        }else{
+            require(withdraw(msg.sender));
+        }
+    }
+    event ReceivedHpb(
+        address payable indexed sender,
+        uint amount
+    );
+    event LockBal(
+        address indexed nodeAddr,
+        address indexed addr,
+        uint indexed lockNum
+    );
+    event withdrawBal(
+        address indexed nodeAddr,
+        address indexed addr,
+        uint indexed lockNum
+    );
+
+    function kill() onlyOwner payable public returns(bool) {
+        selfdestruct(owner);
+        return true;
+    }
+    
+}
---
+++ contract/lockandvote/test/hardhat.config.js
@@ -0,0 +1,29 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    const balance = await network.provider.send("eth_getBalance", [account.address])
+    console.log(account.address, balance);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  networks: {
+    hardhat: {
+      accounts:{
+        accountsBalance:"80000000000000000000000"
+      }
+    }
+  },
+  solidity: "0.5.1",
+};
