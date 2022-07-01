a4ea41a02ef02774fd7d5b633d2d8619056ed46d
sager <sager@hpb.io>
2022-07-01 16:50:47 +0000

    add filter proxy contract and script

---
+++ contract/lockandvote/FilterProxy.sol
@@ -0,0 +1,106 @@
+pragma solidity ^0.5.1;
+import "./owner.sol";
+
+interface IHpbNodes {
+    function fetchAllHolderAddrs() external view returns(address [] memory, address [] memory);
+    function getAllHpbNodes() external view returns (address payable [] memory, bytes32[] memory, bytes32[] memory, bytes32[] memory);
+}
+
+interface IHpbVote {
+    function fetchAllVoteResult() external view returns (address payable[] memory, uint[] memory);
+}
+
+interface IHpbLock {
+    function fetchLockInfoByNodeAddr(address nodeAddr) external view returns (address, uint);
+}
+
+
+contract FilterProxy is Ownable{
+    mapping (address=>bool) blacklist;
+    
+    IHpbNodes hpbnode;
+    IHpbVote  hpbvote;
+    IHpbLock  hpblock;
+
+    constructor () payable public {
+        owner = msg.sender;
+        addAdmin(owner);
+    }
+
+    function setnodecontract(address payable nodeaddr) onlyAdmin public{
+        hpbnode = IHpbNodes(nodeaddr);
+    }
+
+    function setvotecontract(address payable voteaddr) onlyAdmin public{
+        hpbvote = IHpbVote(voteaddr);
+    }
+
+    function setlockcontract(address payable lockaddr) onlyAdmin public{
+        hpblock = IHpbLock(lockaddr);
+    }
+
+    function addInvalidNode(address addr) onlyAdmin public {
+        blacklist[addr] = true;
+    }
+
+    function removeInvalidNode(address addr) onlyAdmin public {
+        blacklist[addr] = false;
+    }
+
+    function getcontract() public view returns(address,address,address){
+        return (address(hpbnode),address(hpblock),address(hpbvote));
+    }
+
+    function getAllHpbNodes(
+    )  public view returns (
+        address payable[] memory,
+        bytes32[] memory,
+        bytes32[] memory,
+        bytes32[] memory
+    ) {
+
+        uint256 count = 0;
+        address payable [] memory _coinbase = new address payable[](1);
+        bytes32[] memory _cid1s = new bytes32[](1);
+        bytes32[] memory _cid2s = new bytes32[](1);
+        bytes32[] memory _hids = new bytes32[](1);
+        (_coinbase, _cid1s, _cid2s, _hids) = hpbnode.getAllHpbNodes();
+
+        for(uint256 i=0; i < _coinbase.length; i++) {
+            if(blacklist[_coinbase[i]] == false) {
+	    	count += 1;
+            }
+        }
+        address payable [] memory _filter_coinbase = new address payable[](count);
+        bytes32[] memory _filter_cid1s = new bytes32[](count);
+        bytes32[] memory _filter_cid2s = new bytes32[](count);
+        bytes32[] memory _filter_hids = new bytes32[](count);
+
+        uint256 index = 0;
+        for(uint256 i=0; i < _coinbase.length; i++) {
+            if(blacklist[_coinbase[i]] == false) {
+                _filter_coinbase[index] = _coinbase[i];
+                _filter_cid1s[index] = _cid1s[i];
+                _filter_cid2s[index] = _cid2s[i];
+                _filter_hids[index] = _hids[i];
+	        index += 1;
+            }
+        }
+        
+        return (_filter_coinbase, _filter_cid1s, _filter_cid2s, _filter_hids);
+    }
+
+    function fetchAllVoteResult() public view returns (
+        address payable[] memory, 
+        uint[] memory
+    ){
+        return hpbvote.fetchAllVoteResult();
+    }
+
+    function fetchAllHolderAddrs() public view returns(
+        address [] memory,
+        address [] memory){
+        return hpbnode.fetchAllHolderAddrs();
+    }
+
+}
---
+++ contract/lockandvote/test/scripts/filterproxy.js
@@ -0,0 +1,96 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+function wait(ms) {
+	return new Promise(resolve =>setTimeout(() =>resolve(), ms));
+};
+
+async function deployFilterProxy(contractMap) {
+	const FilterProxy = await hre.ethers.getContractFactory("FilterProxy");
+	const filterProxy = await FilterProxy.deploy();
+	await filterProxy.deployed();
+	console.log("deploy filterProxy address:", filterProxy.address);
+
+	var tx = await filterProxy.setnodecontract(contractMap.get("hpbnodes").address);
+	await tx.wait();
+	console.log("set node contract succeed");
+
+	tx = await filterProxy.setvotecontract(contractMap.get("hpbvotes").address);
+	await tx.wait();
+	console.log("set vote contract succeed");
+
+	tx = await filterProxy.setlockcontract(contractMap.get("hpblocks").address);
+	await tx.wait();
+	console.log("set lock contract succeed");
+
+	contractMap.set("filterproxy", filterProxy);
+
+	return contractMap;
+}
+
+async function initaddr() {
+	var hpbNodes = "0x451d785A0379E637d17C1C0E96cA150168A5Ab9A";
+	var hpbLocks = "0x74054455F954E1DDAf0694d906e4e68D63a33A18";
+	var hpbVotes = "0x35a3445C0ca0B01B7CEA2F867D762f6410c9e952";
+	var proxy = "0x60a3698bE1493da2065E6F84B2E77B5b5D201D5D";
+	var filterproxy = "0x5D60b3bf2483FB19d0a4FE76Da8857e10AE13201";
+
+	var contractMap = new Map();
+
+	const HpbNodes = await hre.ethers.getContractAt("HpbNodes", hpbNodes);
+	const HpbLocks = await hre.ethers.getContractAt("HpbLock", hpbLocks);
+	const HpbVotes = await hre.ethers.getContractAt("HpbVote", hpbVotes);
+	const HpbProxy = await hre.ethers.getContractAt("Proxy", proxy);
+	const FilterProxy = await hre.ethers.getContractAt("FilterProxy", filterproxy);
+	contractMap.set("hpbnodes", HpbNodes);
+	contractMap.set("hpblocks", HpbLocks);
+	contractMap.set("hpbvotes", HpbVotes);
+	contractMap.set("hpbproxy", HpbProxy);
+	contractMap.set("filterproxy", FilterProxy);
+	return contractMap;
+}
+
+async function addInvalidNode(contract, address) {
+	var tx = await contract.addInvalidNode(address,{gasLimit:1000000, gasPrice:1000000000});
+	await tx.wait();
+	console.log("add invalid node ", address);
+}
+
+async function testGetAllHpbNodes() {
+	var contractMap = await initaddr();
+	//contractMap = await deployFilterProxy(contractMap);
+
+	await addInvalidNode(contractMap.get("filterproxy"),"0xAe43e6D43FD29100fAca6d2AeD1dFBF5EdcBD41B");
+	//await addInvalidNode(contractMap.get("filterproxy"),"0xF583c835961e669556448B80E4e0e491c7eE6C26");
+	{
+		var [coinbases,cid1s,cid2s,hids] = await contractMap.get("filterproxy").getAllHpbNodes();
+		for(var i = 0; i < coinbases.length; i++) {
+			console.log("filter proxy value at index ", i, " = ", coinbases[i], cid1s[i], cid2s[i], hids[i]);
+		}  
+	}
+	{
+		var [coinbases,cid1s,cid2s,hids] = await contractMap.get("hpbproxy").getAllHpbNodes();
+		for(var i = 0; i < coinbases.length; i++) {
+			console.log("hpb proxy value at index ", i, " = ", coinbases[i], cid1s[i], cid2s[i], hids[i]);
+		}
+	}
+
+}
+
+async function main() {
+	await testGetAllHpbNodes();
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
+	});
+
